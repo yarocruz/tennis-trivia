bc702d1827e76017316b2783082ad0b7c649b4bd
yarocruz <yarocruz@gmail.com>
2021-10-13 09:41:47 -0400

    added the logic to make up to 5 guesses

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { Player, uniqueCountries, top100Players } from "../lib/players"
+import { useState } from "react";
 
 type HomeProps = {
   players: Player[];
@@ -6,7 +7,33 @@ type HomeProps = {
 }
 
 export default function Home({ players, countries } : HomeProps) {
-  const player = players[0]
+  const [score, setScore] = useState(0)
+  const [pickedCountry, setPickedCountry] = useState("")
+  const [status, setStatus] = useState(null)
+
+  // state for logic to have 5 tries
+  const [currentStep, setCurrentStep] = useState(0)
+  const [playerData, setPlayerData] = useState(players)
+
+  // current tennis player, which it starts at players[0]
+  const player = playerData[currentStep]
+
+  // function checks if the country selected is the country where current player (players[0]) is from
+  const guessCountry = () => {
+    if (player.country.toLocaleLowerCase() === pickedCountry.toLocaleLowerCase()) {
+      setStatus({ status: "correct", country: player.country})
+      setScore(score + 1)
+    } else {
+      alert('incorrect')
+      setStatus({ status: "incorrect", country: player.country})
+    }
+  }
+
+  const nextPlayer = () => {
+    setPickedCountry("")
+    setCurrentStep(currentStep + 1)
+    setStatus(null)
+  }
 
   return (
     <div className="bg-blue-500">
@@ -15,38 +42,83 @@ export default function Home({ players, countries } : HomeProps) {
           <span className="block">Tennis Trivia - Next.js Netlify</span>
         </h2>
 
-        <div>
-          <p className="mt-4 text-lg leading-6 text-blue-200">
-            What country is the following tennis player from?
-          </p>
-          <h2 className="text-lg font-extrabold text-white my-5">
-            Roger Federer
-          </h2>
+        {/* if we're still within the 5 tennis players render the jsx below */}
+        {player ? (
+            <div>
+              <p className="mt-4 text-lg leading-6 text-blue-200">
+                What country is the following tennis player from?
+              </p>
+              <h2 className="text-lg font-extrabold text-white my-5">
+                {player.full_name}
+              </h2>
 
-          <form>
-            <input
-                list="countries"
-                type="text"
-                className="p-2 outline-none"
-                placeholder="Choose Country"
-            />
-            <datalist id="countries">
-              <option>Switzerland</option>
-            </datalist>
-            <p>
+              {/* conditional here to render elements only if status is truthy, basically once you select a country */}
+              {status && (
+                  <div className="mt-4 text-lg leading-6 text-white">
+                    <p>
+                      You are {status.status}. It is {status.country}
+                    </p>
+                    <p>
+                      <button
+                          autoFocus
+                          onClick={nextPlayer}
+                          className="outline-none mt-8 w-full inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-blue-600 bg-white hover:bg-blue-50 sm:w-auto"
+                      >
+                        Next Player
+                      </button>
+                    </p>
+                  </div>
+              )}
+
+              {/* making sure the input field doesn't show when we select a country with !status*/}
+              {!status && (
+                  <form
+                      onSubmit={(event => {
+                        event.preventDefault()
+                        guessCountry()
+                      })}
+                  >
+                    <input
+                        list="countries"
+                        type="text"
+                        value={pickedCountry}
+                        onChange={(event => setPickedCountry(event.target.value))}
+                        className="p-2 outline-none"
+                        placeholder="Choose Country"
+                    />
+                    <datalist id="countries">
+                      {countries.map((country, i) => (
+                          <option key={i}>{country}</option>
+                      ))}
+                    </datalist>
+                    <p>
+                      <button
+                          className="mt-8 w-full inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-blue-600 bg-white hover:bg-blue-50 sm:w-auto"
+                          type="submit"
+                      >
+                        Guess
+                      </button>
+                    </p>
+                  </form>
+              )}
+
+
+              <p className="mt-4 text-lg leading-6 text-white">
+                <strong>Current score:</strong> {score}
+              </p>
+            </div>
+        ) : (
+           // else if player is undefined, falsy, render this jsx
+            <div>
               <button
-                  className="mt-8 w-full inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-blue-600 bg-white hover:bg-blue-50 sm:w-auto"
-                  type="submit"
+                  autoFocus
+                  className="outline-none mt-8 w-full inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50 sm:w-auto"
               >
-                Guess
+                Play Again
               </button>
-            </p>
-          </form>
+            </div>
+        )}
 
-          <p className="mt-4 text-lg leading-6 text-white">
-            <strong>Current score:</strong> 0
-          </p>
-        </div>
 
       </div>
     </div>
@@ -55,6 +127,7 @@ export default function Home({ players, countries } : HomeProps) {
 
 export async function getServerSideProps() {
   const randomizedPlayers = top100Players.sort((a, b) => 0.5 - Math.random())
+  // cut it down to just 5 tennis players
   const players = randomizedPlayers.slice(0, 5)
 
   return {
